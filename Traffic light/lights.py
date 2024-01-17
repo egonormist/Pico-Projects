7a023d95e6115f91c3f6332d656d23e3885f161a
imbwaga-gearbox <imbwaga@students.uonbi.ac.ke>
2024-01-17 12:12:12 +0300

    Traffic

---
+++ Traffic light/lights.py
@@ -0,0 +1,20 @@
+from machine import Pin
+from utime import sleep
+red = Pin(0,Pin.OUT)
+amber = Pin(16,Pin.OUT)
+green = Pin(2,Pin.OUT)
+while True:
+    green.on()
+    sleep(5)
+    green.off()
+    amber.on()
+    sleep(3)
+    amber.off()
+    red.on()
+    sleep(5)
+    amber.on()
+    sleep(3)
+    red.off()
+    amber.off()
+    
+    
