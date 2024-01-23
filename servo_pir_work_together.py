606a5575ba0ad6d8d733acc985276688526aae66
imbwaga-gearbox <imbwaga@students.uonbi.ac.ke>
2024-01-23 12:37:58 +0300

    =new projects

---
+++ servo_pir_work_together.py
@@ -0,0 +1,24 @@
+from machine import Pin, PWM
+from utime import sleep
+
+servo =PWM(Pin(16))
+servo.freq(50)
+motion =Pin(17,Pin.IN)
+
+def motor(prop):
+    finalvalue =(6554/180)*prop + 1638
+    print (int(finalvalue))
+    return int(finalvalue)
+
+def movement():
+    if motion.value() ==1:
+        servo.duty_u16(motor(45))
+        sleep(2)
+    
+    else:
+        servo.duty_u16(motor(0))
+        sleep(1)
+    
+while True:
+    movement()
+    
